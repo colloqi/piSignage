f01c2cd9ece5099c9c65531ee9ffaf1521045a10
navaneetharaopy <navaneetha@ariem2.com>
2014-03-19 17:57:12 +0530

    example

--- misc/start.sh
+++ misc/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #midori -e Fullscreen -a www.bbc.co.uk &
-#uzbl www.yahoo.com &
+uzbl www.yahoo.com &
 unclutter -idle 5 &
 cd ~/piSignage
 #/opt/node/bin/node ~/piSignage/server.js
 #forever start -l /var/log/forever.log -o /var/log/forever-out.log -e/var/log/forver-err.log ~/piSignage/server.js
-forever start -l /tmp/forever.log ~/piSignage/server.js
+#forever start -l /tmp/forever.log ~/piSignage/server.js
