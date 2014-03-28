84ede9b17efda15da88e910bb58c3e72b53bf577
navaneetharaopy <navaneetha@ariem2.com>
2014-03-28 16:48:13 +0530

    forever not works. So node.js on startup

--- misc/start.sh
+++ misc/start.sh
@@ -3,6 +3,6 @@
 #uzbl www.yahoo.com &
 unclutter -idle 5 &
 cd ~/piSignage
-#/opt/node/bin/node ~/piSignage/server.js
+/opt/node/bin/node ~/piSignage/server.js
 #forever start -l /var/log/forever.log -o /var/log/forever-out.log -e/var/log/forver-err.log ~/piSignage/server.js
-forever start -l /tmp/forever.log ~/piSignage/server.js
+#forever start -l /tmp/forever.log ~/piSignage/server.js
